d2c3a60ac0c0030a5e0e588da2439ba10238d8fb
Joan <joandenchev@gmail.com>
2024-12-23 01:33:30 +0200

    Init 2

--- go.js
+++ go.js
@@ -2,7 +2,7 @@ import express from 'express'
 const go = express()
 
 go.get('/', (req, res) => {
-    res.send('<h1>Oktai ti si mnogo sladuk! ❤️</h1>')
+    res.send('<h1>Дарба карта включи! \n Максимум пирус!</h1>')
 })
 
 go.listen(process.env.PORT || 3105, function () {
